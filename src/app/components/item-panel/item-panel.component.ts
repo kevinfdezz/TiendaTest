9eec51d4df4ed7b10f820e7e7800063313ecf82b
Kevin <bycaozz@gmail.com>
2025-01-04 14:55:17 +0100

    Create some components and change angular.json

---
+++ src/app/components/item-panel/item-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-panel',
+  imports: [],
+  templateUrl: './item-panel.component.html',
+  styleUrl: './item-panel.component.css'
+})
+export class ItemPanelComponent {
+
+}
